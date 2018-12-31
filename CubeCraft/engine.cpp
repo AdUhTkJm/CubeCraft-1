7c6a4b573e99a6b72e8f6cdcc3c25ee4eeb87eeb
Moore <moore_smart@icloud.com>
2019-01-01 01:07:21 +0800

    Update

--- CubeCraft/engine.cpp
+++ CubeCraft/engine.cpp
@@ -1,7 +1,14 @@
 #include "engine.h"
 #include <SDL/SDL.h>
+#include <stdexcept>
 
+/*SDL*/
 void cc::engine::init()
 {
-
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		throw std::runtime_error("Cannot init SDL.");
+	}
 }
